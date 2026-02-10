47fcc079ed1770da64bf7f9058c5e425bbb82c05
zer0ascii <samuel.schnoedt@web.de>
2026-02-10 15:08:40 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ TOKEN = os.getenv("DISCORD_TOKEN")  # GitHub Secret
 
 intents = discord.Intents.default()
 intents.message_content = True
-bot = commands.Bot(command_prefix="!", intents=intents)
+bot = commands.Bot(command_prefix="/", intents=intents)
 
 @bot.event
 async def on_ready():
